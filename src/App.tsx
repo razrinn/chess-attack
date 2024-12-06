7a61f4118200bb0058b8a470060d6063eb0d4a7f
Ray Azrin Karim <ray.karim@stockbit.com>
2024-12-06 10:47:27 +0700

    add github footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Board } from './components/Board';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className='min-h-screen w-full bg-gray-100 flex items-center justify-center p-4'>
-      <div className='max-w-full overflow-auto bg-white rounded-lg shadow-lg p-4'>
-        <Board />
+    <div className='min-h-screen w-full bg-gray-100 flex flex-col'>
+      <div className='flex-1 flex items-center justify-center p-4'>
+        <div className='max-w-full overflow-auto bg-white rounded-lg shadow-lg p-4'>
+          <Board />
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
